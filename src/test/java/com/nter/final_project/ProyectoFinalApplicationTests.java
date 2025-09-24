5205488b4ced11927871bab05af277781db55813
Antonio <memecito@users.noreply.github.com>
2025-09-24 14:43:48 +0200

    inicio proyecto

---
+++ src/test/java/com/nter/final_project/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nter.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
