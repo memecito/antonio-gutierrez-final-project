940da876f957ef565d88f75c86a58ab267b83900
Antonio <memecito@users.noreply.github.com>
2025-09-25 09:18:28 +0200

    creacion estructura basica de los controllers

---
+++ src/main/java/com/nter/final_project/presentation/controllers/CountryController.java
@@ -0,0 +1,37 @@
+package com.nter.final_project.presentation.controllers;
+
+import com.nter.final_project.presentation.dto.apiuser.ApiUserInDto;
+import com.nter.final_project.presentation.dto.country.CountryInDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/countries")
+@RequiredArgsConstructor
+public class CountryController {
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok("get all user, logica por hacer");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        return ResponseEntity.ok("get by id, logica por hacer");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> created(@RequestBody CountryInDto country) {
+        return ResponseEntity.ok("post user, logica por hacer");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody CountryInDto country) {
+        return ResponseEntity.ok("update, logica por hacer");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleted(@PathVariable Long id){
+        return ResponseEntity.ok("delete user, logica por hacer");
+    }
+}
---
+++ src/main/java/com/nter/final_project/presentation/controllers/ProductController.java
@@ -0,0 +1,38 @@
+package com.nter.final_project.presentation.controllers;
+
+import com.nter.final_project.presentation.dto.apiuser.ApiUserInDto;
+import com.nter.final_project.presentation.dto.country.CountryInDto;
+import com.nter.final_project.presentation.dto.product.ProductInDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok("get all user, logica por hacer");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        return ResponseEntity.ok("get by id, logica por hacer");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> created(@RequestBody ProductInDto product) {
+        return ResponseEntity.ok("post user, logica por hacer");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody ProductInDto product) {
+        return ResponseEntity.ok("update, logica por hacer");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleted(@PathVariable Long id){
+        return ResponseEntity.ok("delete user, logica por hacer");
+    }
+}
