1c699edd46519e272b3e9e292d621cdb95630ec6
memecito <memecito@users.noreply.github.com>
2025-09-26 13:22:00 +0200

    Continaumod añadiendo funcionalidad a los controladors

--- src/main/java/com/nter/final_project/presentation/controllers/ApiUserController.java
+++ src/main/java/com/nter/final_project/presentation/controllers/ApiUserController.java
@@ -3,6 +3,7 @@ package com.nter.final_project.presentation.controllers;
 import com.nter.final_project.application.mappers.ApiUserMapped;
 import com.nter.final_project.application.services.ApiUserService;
 import com.nter.final_project.persistence.entity.ApiUser;
+import com.nter.final_project.presentation.dto.BasicResponseDto;
 import com.nter.final_project.presentation.dto.apiuser.ApiUserInDto;
 import com.nter.final_project.presentation.dto.apiuser.ApiUserOutDto;
 import com.nter.final_project.presentation.dto.apiuser.ApiUserOutDtoMini;
@@ -10,6 +11,7 @@ import com.nter.final_project.presentation.dto.country.CountryInDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,9 +31,8 @@ public class ApiUserController {
             @RequestParam(defaultValue = "0", required = false) int pageNumber,
             @RequestParam(defaultValue = "10", required = false) int pageSize
     ) {
-        Page<ApiUser> userPage= apiUserService.getAll(pageNumber,pageSize);
-        Page<ApiUserOutDtoMini> userMiniPage=  userPage.map(apiUserMapped::toDtoMini);
-        return ResponseEntity.ok(userMiniPage);
+
+        return ResponseEntity.ok(apiUserService.getAll(pageNumber,pageSize).map(apiUserMapped::toDtoMini));
     }
 
     @GetMapping("/{id}")
@@ -57,6 +58,9 @@ public class ApiUserController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleted(@PathVariable Long id) {
         apiUserService.deleted(id);
-        return ResponseEntity.ok("delete user, logica por hacer");
+        return ResponseEntity.ok(BasicResponseDto.builder()
+                .status(HttpStatus.OK.value())
+                .message("usuario borrado")
+                .build());
     }
 }
--- src/main/java/com/nter/final_project/presentation/controllers/ProductController.java
+++ src/main/java/com/nter/final_project/presentation/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package com.nter.final_project.presentation.controllers;
 
+import com.nter.final_project.application.mappers.ProductMapped;
+import com.nter.final_project.application.services.ProductService;
+import com.nter.final_project.presentation.dto.BasicResponseDto;
 import com.nter.final_project.presentation.dto.apiuser.ApiUserInDto;
 import com.nter.final_project.presentation.dto.country.CountryInDto;
 import com.nter.final_project.presentation.dto.product.ProductInDto;
+import com.nter.final_project.presentation.dto.product.ProductOutDtoMIni;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +17,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+    private final ProductMapped productMapped;
     @GetMapping
-    public ResponseEntity<?> getAll(@RequestParam(defaultValue = "0", required = false) int pageNumber,
-                                    @RequestParam(defaultValue = "10", required = false) int pageSize) {
-        return ResponseEntity.ok("get all user, logica por hacer");
+    public ResponseEntity<Page<ProductOutDtoMIni>> getAll(@RequestParam(defaultValue = "0", required = false) int pageNumber,
+                                                          @RequestParam(defaultValue = "10", required = false) int pageSize) {
+        return ResponseEntity.ok(productService.getAll(pageNumber, pageSize).map(productMapped::toDtoMini));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable Long id) {
-        return ResponseEntity.ok("get by id, logica por hacer");
+    public ResponseEntity<ProductOutDtoMIni> getById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(productMapped.toDto(productService.getById(id)));
     }
 
     @PostMapping
@@ -34,6 +44,10 @@ public class ProductController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleted(@PathVariable Long id){
-        return ResponseEntity.ok("delete user, logica por hacer");
+        productService.deleted(id);
+        return ResponseEntity.ok(BasicResponseDto.builder()
+                .status(HttpStatus.OK.value())
+                .message("usuario borrado")
+                .build());
     }
 }
