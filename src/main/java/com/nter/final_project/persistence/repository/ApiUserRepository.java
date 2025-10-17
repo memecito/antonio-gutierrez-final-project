dfd0da8e71638975c7078c549a3cdbe26f4682a1
memecito <memecito@users.noreply.github.com>
2025-10-17 10:18:45 +0200

    docker

--- src/main/java/com/nter/final_project/persistence/repository/ApiUserRepository.java
+++ src/main/java/com/nter/final_project/persistence/repository/ApiUserRepository.java
@@ -4,10 +4,11 @@ import com.nter.final_project.persistence.entity.ApiUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface ApiUserRepository extends JpaRepository<ApiUser, Long> {
     Optional<ApiUser> findByEmail(String email);
 
--- src/main/java/com/nter/final_project/persistence/repository/OrderProductRepository.java
+++ src/main/java/com/nter/final_project/persistence/repository/OrderProductRepository.java
@@ -3,6 +3,8 @@ package com.nter.final_project.persistence.repository;
 import com.nter.final_project.persistence.entity.OrderProduct;
 import com.nter.final_project.persistence.entity.OrderProductId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderProductRepository extends JpaRepository<OrderProduct, OrderProductId> {
 }
